312d4a24d9008be957dccb07e2e6f926141da738
du023101092-sudo <du023101092@tgu.edu.vn>
2026-04-26 16:31:08 +0700

    Update frmGiangVien.cs

--- QLPM_PTH/frmGiangVien.cs
+++ QLPM_PTH/frmGiangVien.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -37,6 +37,66 @@ namespace QLPM_PTH
             String gv = "Giảng Viên";
 
             materialLabel1.Text = $"{role} | {fullname} | {gv}";
+        }
+
+        private void materialCard2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void materialLabel2_Click(object sender, EventArgs e)
+        {
+            frmLichPhongHoc frmLich = new frmLichPhongHoc();
+            frmLich.Show();
+
+        }
+
+        private void materialCard2_Click(object sender, EventArgs e)
+        {
+            frmLichPhongHoc frmLich = new frmLichPhongHoc();
+            frmLich.Show();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            frmLichPhongHoc frmLich = new frmLichPhongHoc();
+            frmLich.Show();
+        }
+
+        private void materialCard3_Click(object sender, EventArgs e)
+        {
+            frmQLYC frmYc = new frmQLYC();
+            frmYc.Show();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            frmQLYC frmYc = new frmQLYC();
+            frmYc.Show();
+        }
+
+        private void materialLabel3_Click(object sender, EventArgs e)
+        {
+            frmQLYC frmYc = new frmQLYC();
+            frmYc.Show();
+        }
+
+        private void materialCard4_Click(object sender, EventArgs e)
+        {
+            frmSuDungThietBi frmSd = new frmSuDungThietBi();
+            frmSd.Show();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            frmSuDungThietBi frmSd = new frmSuDungThietBi();
+            frmSd.Show();
+        }
+
+        private void materialLabel4_Click(object sender, EventArgs e)
+        {
+            frmSuDungThietBi frmSd = new frmSuDungThietBi();
+            frmSd.Show();
         }
     }
 }
