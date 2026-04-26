54cef6a67da5c62a5a1ecb412e75e6576fd8be49
du023101092-sudo <du023101092@tgu.edu.vn>
2026-04-26 16:36:31 +0700

    Update frmLichPhongHoc.cs

--- QLPM_PTH/frmLichPhongHoc.cs
+++ QLPM_PTH/frmLichPhongHoc.cs
@@ -1,14 +1,15 @@
-﻿using System;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MaterialSkin;
-using MaterialSkin.Controls;
 
 namespace QLPM_PTH
 {
@@ -17,6 +18,49 @@ namespace QLPM_PTH
         public frmLichPhongHoc()
         {
             InitializeComponent();
+            
+        }
+        string strCon = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=QLPM;Integrated Security=True";
+        SqlConnection sqlCon = null;
+
+        private void btnDangKy_Click(object sender, EventArgs e)
+        {
+            frmQLYC frmYc = new frmQLYC();
+            frmYc.Show();
+        }
+
+        private void panelHeader_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanelContent_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnBaoLoi_Click(object sender, EventArgs e)
+        {
+            string strCon = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=QLPM;Integrated Security=True";
+
+            using (SqlConnection sqlCon = new SqlConnection(strCon))
+            {
+                try
+                {
+                    sqlCon.Open(); // Thử mở cổng kết nối
+                    MessageBox.Show("Kết nối thành công! Bạn đã chạm được vào database SQLPM.");
+                }
+                catch (Exception ex)
+                {
+                    // Nếu có lỗi (sai tên server, sai tên DB...), nó sẽ hiện ở đây
+                    MessageBox.Show("Thất bại rồi! Lỗi cụ thể là: " + ex.Message);
+                }
+            }
+        }
+
+        private void frmLichPhongHoc_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
