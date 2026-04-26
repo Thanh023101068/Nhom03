e8124a7146c4c48d168b854a43bf008a30e0a218
du023101092-sudo <du023101092@tgu.edu.vn>
2026-04-26 16:35:17 +0700

    Update frmLichPhongHoc.Designer.cs

--- QLPM_PTH/frmLichPhongHoc.Designer.cs
+++ QLPM_PTH/frmLichPhongHoc.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace QLPM_PTH
+namespace QLPM_PTH
 {
     partial class frmLichPhongHoc
     {
@@ -56,11 +56,11 @@
             this.panelHeader.Controls.Add(this.lblUserInfo);
             this.panelHeader.Controls.Add(this.picAvatar);
             this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelHeader.Location = new System.Drawing.Point(4, 79);
-            this.panelHeader.Margin = new System.Windows.Forms.Padding(4);
+            this.panelHeader.Location = new System.Drawing.Point(3, 64);
             this.panelHeader.Name = "panelHeader";
-            this.panelHeader.Size = new System.Drawing.Size(1725, 74);
+            this.panelHeader.Size = new System.Drawing.Size(1034, 60);
             this.panelHeader.TabIndex = 2;
+            this.panelHeader.Paint += new System.Windows.Forms.PaintEventHandler(this.panelHeader_Paint);
             // 
             // lblUserInfo
             // 
@@ -68,8 +68,7 @@
             this.lblUserInfo.AutoSize = true;
             this.lblUserInfo.Depth = 0;
             this.lblUserInfo.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.lblUserInfo.Location = new System.Drawing.Point(759, 27);
-            this.lblUserInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUserInfo.Location = new System.Drawing.Point(439, 22);
             this.lblUserInfo.MouseState = MaterialSkin.MouseState.HOVER;
             this.lblUserInfo.Name = "lblUserInfo";
             this.lblUserInfo.Size = new System.Drawing.Size(21, 19);
@@ -80,10 +79,9 @@
             // 
             this.picAvatar.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.picAvatar.Image = ((System.Drawing.Image)(resources.GetObject("picAvatar.Image")));
-            this.picAvatar.Location = new System.Drawing.Point(703, 14);
-            this.picAvatar.Margin = new System.Windows.Forms.Padding(4);
+            this.picAvatar.Location = new System.Drawing.Point(397, 11);
             this.picAvatar.Name = "picAvatar";
-            this.picAvatar.Size = new System.Drawing.Size(48, 47);
+            this.picAvatar.Size = new System.Drawing.Size(36, 38);
             this.picAvatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picAvatar.TabIndex = 0;
             this.picAvatar.TabStop = false;
@@ -93,10 +91,9 @@
             this.pnMain.Controls.Add(this.tableLayoutPanelContent);
             this.pnMain.Controls.Add(this.pnRightActions);
             this.pnMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnMain.Location = new System.Drawing.Point(4, 153);
-            this.pnMain.Margin = new System.Windows.Forms.Padding(4);
+            this.pnMain.Location = new System.Drawing.Point(3, 124);
             this.pnMain.Name = "pnMain";
-            this.pnMain.Size = new System.Drawing.Size(1725, 828);
+            this.pnMain.Size = new System.Drawing.Size(1034, 513);
             this.pnMain.TabIndex = 3;
             // 
             // tableLayoutPanelContent
@@ -107,13 +104,13 @@
             this.tableLayoutPanelContent.Controls.Add(this.cardSearch, 0, 0);
             this.tableLayoutPanelContent.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanelContent.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanelContent.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanelContent.Name = "tableLayoutPanelContent";
             this.tableLayoutPanelContent.RowCount = 2;
-            this.tableLayoutPanelContent.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 135F));
+            this.tableLayoutPanelContent.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 110F));
             this.tableLayoutPanelContent.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanelContent.Size = new System.Drawing.Size(1258, 828);
+            this.tableLayoutPanelContent.Size = new System.Drawing.Size(684, 513);
             this.tableLayoutPanelContent.TabIndex = 0;
+            this.tableLayoutPanelContent.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanelContent_Paint);
             // 
             // cardGrid
             // 
@@ -122,12 +119,12 @@
             this.cardGrid.Depth = 0;
             this.cardGrid.Dock = System.Windows.Forms.DockStyle.Fill;
             this.cardGrid.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.cardGrid.Location = new System.Drawing.Point(19, 152);
-            this.cardGrid.Margin = new System.Windows.Forms.Padding(19, 17, 19, 17);
+            this.cardGrid.Location = new System.Drawing.Point(14, 124);
+            this.cardGrid.Margin = new System.Windows.Forms.Padding(14);
             this.cardGrid.MouseState = MaterialSkin.MouseState.HOVER;
             this.cardGrid.Name = "cardGrid";
-            this.cardGrid.Padding = new System.Windows.Forms.Padding(19, 17, 19, 17);
-            this.cardGrid.Size = new System.Drawing.Size(1220, 659);
+            this.cardGrid.Padding = new System.Windows.Forms.Padding(14);
+            this.cardGrid.Size = new System.Drawing.Size(656, 375);
             this.cardGrid.TabIndex = 1;
             // 
             // lstLich
@@ -143,16 +140,14 @@
             this.lstLich.Depth = 0;
             this.lstLich.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lstLich.FullRowSelect = true;
-            this.lstLich.GridLines = true;
             this.lstLich.HideSelection = false;
-            this.lstLich.Location = new System.Drawing.Point(19, 17);
-            this.lstLich.Margin = new System.Windows.Forms.Padding(4);
-            this.lstLich.MinimumSize = new System.Drawing.Size(267, 123);
+            this.lstLich.Location = new System.Drawing.Point(14, 14);
+            this.lstLich.MinimumSize = new System.Drawing.Size(200, 100);
             this.lstLich.MouseLocation = new System.Drawing.Point(-1, -1);
             this.lstLich.MouseState = MaterialSkin.MouseState.OUT;
             this.lstLich.Name = "lstLich";
             this.lstLich.OwnerDraw = true;
-            this.lstLich.Size = new System.Drawing.Size(1182, 625);
+            this.lstLich.Size = new System.Drawing.Size(628, 347);
             this.lstLich.TabIndex = 0;
             this.lstLich.UseCompatibleStateImageBehavior = false;
             this.lstLich.View = System.Windows.Forms.View.Details;
@@ -188,12 +183,12 @@
             this.cardSearch.Depth = 0;
             this.cardSearch.Dock = System.Windows.Forms.DockStyle.Fill;
             this.cardSearch.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.cardSearch.Location = new System.Drawing.Point(19, 17);
-            this.cardSearch.Margin = new System.Windows.Forms.Padding(19, 17, 19, 17);
+            this.cardSearch.Location = new System.Drawing.Point(14, 14);
+            this.cardSearch.Margin = new System.Windows.Forms.Padding(14);
             this.cardSearch.MouseState = MaterialSkin.MouseState.HOVER;
             this.cardSearch.Name = "cardSearch";
-            this.cardSearch.Padding = new System.Windows.Forms.Padding(19, 17, 19, 17);
-            this.cardSearch.Size = new System.Drawing.Size(1220, 101);
+            this.cardSearch.Padding = new System.Windows.Forms.Padding(14);
+            this.cardSearch.Size = new System.Drawing.Size(656, 82);
             this.cardSearch.TabIndex = 0;
             // 
             // btnTimKiem
@@ -204,12 +199,12 @@
             this.btnTimKiem.Depth = 0;
             this.btnTimKiem.HighEmphasis = true;
             this.btnTimKiem.Icon = ((System.Drawing.Image)(resources.GetObject("btnTimKiem.Icon")));
-            this.btnTimKiem.Location = new System.Drawing.Point(732, 26);
-            this.btnTimKiem.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.btnTimKiem.Location = new System.Drawing.Point(549, 21);
+            this.btnTimKiem.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.btnTimKiem.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnTimKiem.Name = "btnTimKiem";
             this.btnTimKiem.NoAccentTextColor = System.Drawing.Color.Empty;
-            this.btnTimKiem.Size = new System.Drawing.Size(160, 52);
+            this.btnTimKiem.Size = new System.Drawing.Size(120, 42);
             this.btnTimKiem.TabIndex = 4;
             this.btnTimKiem.Text = "TÌM KIẾM";
             this.btnTimKiem.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
@@ -221,10 +216,9 @@
             this.dtpNgay.CustomFormat = "dd/MM/yyyy";
             this.dtpNgay.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtpNgay.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpNgay.Location = new System.Drawing.Point(505, 34);
-            this.dtpNgay.Margin = new System.Windows.Forms.Padding(4);
+            this.dtpNgay.Location = new System.Drawing.Point(379, 28);
             this.dtpNgay.Name = "dtpNgay";
-            this.dtpNgay.Size = new System.Drawing.Size(199, 34);
+            this.dtpNgay.Size = new System.Drawing.Size(150, 29);
             this.dtpNgay.TabIndex = 3;
             // 
             // lblNgay
@@ -232,8 +226,7 @@
             this.lblNgay.AutoSize = true;
             this.lblNgay.Depth = 0;
             this.lblNgay.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.lblNgay.Location = new System.Drawing.Point(385, 42);
-            this.lblNgay.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNgay.Location = new System.Drawing.Point(289, 34);
             this.lblNgay.MouseState = MaterialSkin.MouseState.HOVER;
             this.lblNgay.Name = "lblNgay";
             this.lblNgay.Size = new System.Drawing.Size(81, 19);
@@ -249,7 +242,7 @@
             this.cmbPhong.DropDownHeight = 174;
             this.cmbPhong.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbPhong.DropDownWidth = 121;
-            this.cmbPhong.Font = new System.Drawing.Font("Roboto Medium", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.cmbPhong.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
             this.cmbPhong.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.cmbPhong.FormattingEnabled = true;
             this.cmbPhong.IntegralHeight = false;
@@ -259,12 +252,11 @@
             "Phòng A1",
             "Phòng A2",
             "Phòng A3"});
-            this.cmbPhong.Location = new System.Drawing.Point(160, 21);
-            this.cmbPhong.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbPhong.Location = new System.Drawing.Point(120, 17);
             this.cmbPhong.MaxDropDownItems = 4;
             this.cmbPhong.MouseState = MaterialSkin.MouseState.OUT;
             this.cmbPhong.Name = "cmbPhong";
-            this.cmbPhong.Size = new System.Drawing.Size(199, 49);
+            this.cmbPhong.Size = new System.Drawing.Size(150, 49);
             this.cmbPhong.StartIndex = 0;
             this.cmbPhong.TabIndex = 1;
             this.cmbPhong.UseAccent = false;
@@ -274,8 +266,7 @@
             this.lblPhong.AutoSize = true;
             this.lblPhong.Depth = 0;
             this.lblPhong.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.lblPhong.Location = new System.Drawing.Point(29, 42);
-            this.lblPhong.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPhong.Location = new System.Drawing.Point(22, 34);
             this.lblPhong.MouseState = MaterialSkin.MouseState.HOVER;
             this.lblPhong.Name = "lblPhong";
             this.lblPhong.Size = new System.Drawing.Size(91, 19);
@@ -286,11 +277,10 @@
             // 
             this.pnRightActions.Controls.Add(this.cardActions);
             this.pnRightActions.Dock = System.Windows.Forms.DockStyle.Right;
-            this.pnRightActions.Location = new System.Drawing.Point(1258, 0);
-            this.pnRightActions.Margin = new System.Windows.Forms.Padding(4);
+            this.pnRightActions.Location = new System.Drawing.Point(684, 0);
             this.pnRightActions.Name = "pnRightActions";
-            this.pnRightActions.Padding = new System.Windows.Forms.Padding(0, 17, 19, 17);
-            this.pnRightActions.Size = new System.Drawing.Size(467, 828);
+            this.pnRightActions.Padding = new System.Windows.Forms.Padding(0, 14, 14, 14);
+            this.pnRightActions.Size = new System.Drawing.Size(350, 513);
             this.pnRightActions.TabIndex = 1;
             // 
             // cardActions
@@ -302,12 +292,12 @@
             this.cardActions.Depth = 0;
             this.cardActions.Dock = System.Windows.Forms.DockStyle.Fill;
             this.cardActions.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.cardActions.Location = new System.Drawing.Point(0, 17);
-            this.cardActions.Margin = new System.Windows.Forms.Padding(19, 17, 19, 17);
+            this.cardActions.Location = new System.Drawing.Point(0, 14);
+            this.cardActions.Margin = new System.Windows.Forms.Padding(14);
             this.cardActions.MouseState = MaterialSkin.MouseState.HOVER;
             this.cardActions.Name = "cardActions";
-            this.cardActions.Padding = new System.Windows.Forms.Padding(19, 17, 19, 17);
-            this.cardActions.Size = new System.Drawing.Size(448, 794);
+            this.cardActions.Padding = new System.Windows.Forms.Padding(14);
+            this.cardActions.Size = new System.Drawing.Size(336, 485);
             this.cardActions.TabIndex = 0;
             // 
             // btnBaoLoi
@@ -318,17 +308,18 @@
             this.btnBaoLoi.Depth = 0;
             this.btnBaoLoi.HighEmphasis = true;
             this.btnBaoLoi.Icon = ((System.Drawing.Image)(resources.GetObject("btnBaoLoi.Icon")));
-            this.btnBaoLoi.Location = new System.Drawing.Point(23, 178);
-            this.btnBaoLoi.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.btnBaoLoi.Location = new System.Drawing.Point(17, 145);
+            this.btnBaoLoi.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.btnBaoLoi.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnBaoLoi.Name = "btnBaoLoi";
             this.btnBaoLoi.NoAccentTextColor = System.Drawing.Color.Empty;
-            this.btnBaoLoi.Size = new System.Drawing.Size(403, 62);
+            this.btnBaoLoi.Size = new System.Drawing.Size(302, 50);
             this.btnBaoLoi.TabIndex = 2;
             this.btnBaoLoi.Text = "BÁO CÁO LỖI THIẾT BỊ";
             this.btnBaoLoi.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Outlined;
             this.btnBaoLoi.UseAccentColor = false;
             this.btnBaoLoi.UseVisualStyleBackColor = true;
+            this.btnBaoLoi.Click += new System.EventHandler(this.btnBaoLoi_Click);
             // 
             // btnDangKy
             // 
@@ -338,17 +329,18 @@
             this.btnDangKy.Depth = 0;
             this.btnDangKy.HighEmphasis = true;
             this.btnDangKy.Icon = ((System.Drawing.Image)(resources.GetObject("btnDangKy.Icon")));
-            this.btnDangKy.Location = new System.Drawing.Point(23, 91);
-            this.btnDangKy.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.btnDangKy.Location = new System.Drawing.Point(17, 74);
+            this.btnDangKy.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.btnDangKy.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnDangKy.Name = "btnDangKy";
             this.btnDangKy.NoAccentTextColor = System.Drawing.Color.Empty;
-            this.btnDangKy.Size = new System.Drawing.Size(403, 62);
+            this.btnDangKy.Size = new System.Drawing.Size(302, 50);
             this.btnDangKy.TabIndex = 1;
             this.btnDangKy.Text = "ĐĂNG KÝ PHÒNG MỚI";
             this.btnDangKy.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
             this.btnDangKy.UseAccentColor = false;
             this.btnDangKy.UseVisualStyleBackColor = true;
+            this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
             // 
             // lblThaoTac
             // 
@@ -356,8 +348,7 @@
             this.lblThaoTac.Depth = 0;
             this.lblThaoTac.Font = new System.Drawing.Font("Roboto Medium", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
             this.lblThaoTac.FontType = MaterialSkin.MaterialSkinManager.fontType.H6;
-            this.lblThaoTac.Location = new System.Drawing.Point(23, 21);
-            this.lblThaoTac.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblThaoTac.Location = new System.Drawing.Point(17, 17);
             this.lblThaoTac.MouseState = MaterialSkin.MouseState.HOVER;
             this.lblThaoTac.Name = "lblThaoTac";
             this.lblThaoTac.Size = new System.Drawing.Size(171, 24);
@@ -366,18 +357,17 @@
             // 
             // frmLichPhongHoc
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1733, 985);
+            this.ClientSize = new System.Drawing.Size(1040, 640);
             this.Controls.Add(this.pnMain);
             this.Controls.Add(this.panelHeader);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "frmLichPhongHoc";
-            this.Padding = new System.Windows.Forms.Padding(4, 79, 4, 4);
             this.Sizable = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LỊCH PHÒNG HỌC THỰC HÀNH";
+            this.Load += new System.EventHandler(this.frmLichPhongHoc_Load);
             this.panelHeader.ResumeLayout(false);
             this.panelHeader.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picAvatar)).EndInit();
