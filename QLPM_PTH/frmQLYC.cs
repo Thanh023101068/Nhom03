756dea8b0c7ecceb15548ad0dd881fd3ed92cf18
du023101092-sudo <du023101092@tgu.edu.vn>
2026-04-26 16:43:31 +0700

    Update frmQLYC.cs

--- QLPM_PTH/frmQLYC.cs
+++ QLPM_PTH/frmQLYC.cs
@@ -1,22 +1,81 @@
-﻿using System;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MaterialSkin;
-using MaterialSkin.Controls;
 
 namespace QLPM_PTH
 {
     public partial class frmQLYC : MaterialForm
     {
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=QLPM;Integrated Security=True";
         public frmQLYC()
         {
             InitializeComponent();
-        }        
+        }
+        private void LoadTest()
+        {
+           
+            string query = "SELECT MaDK, MucDich, MaPhong, NgaySuDung, TrangThaiDuyet FROM DANGKY_PHONG";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    
+                    conn.Open();
+
+                   
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    
+                    dgvYeuCau.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show("Lỗi kết nối CSDL: " + ex.Message, "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnTaoDatPhong_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelHeader_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+     
+        private void tableLayoutPanelContent_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void frmQLYC_Load(object sender, EventArgs e)
+        {
+            LoadTest();
+        }
+
+        private void cardSearch_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dgvYeuCau_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
