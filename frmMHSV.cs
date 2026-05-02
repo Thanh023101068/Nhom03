659a5977789bd9e4f0758e3909556096c27a8d59
Huy Luong Thanh <huy020101069@outlook.com>
2026-05-02 15:43:06 +0700

    LichPhong

--- frmMHSV.cs
+++ frmMHSV.cs
@@ -29,14 +29,65 @@ namespace MHSV
                 Primary.Indigo500,
                 Accent.Blue700,
                 TextShade.WHITE
+//<<<<<<< HEAD
                
-                );
+  //              );
+//=======
+            );
 
+            // =========================================================
+            // 1. LIÊN KẾT: XEM LỊCH PHÒNG MÁY (cardSchedule)
+            // =========================================================
+            cardSchedule.Click += CardSchedule_Click;
+            materialLabel1.Click += CardSchedule_Click; // Click vào chữ
+            pictureBox2.Click += CardSchedule_Click;    // Click vào hình
+
+            // =========================================================
+            // 2. LIÊN KẾT: ĐĂNG KÝ SỬ DỤNG PHÒNG (cardRegister)
+            // =========================================================
+            cardRegister.Click += CardRegister_Click;
+            materialLabel2.Click += CardRegister_Click;
+            pictureBox3.Click += CardRegister_Click;
+
+            // =========================================================
+            // 3. LIÊN KẾT: BÁO CÁO LỖI THIẾT BỊ (cardReport)
+            // =========================================================
+            cardReport.Click += CardReport_Click;
+            materialLabel3.Click += CardReport_Click;
+            pictureBox4.Click += CardReport_Click;
         }
-       
-        private void Form1_Load(object sender, EventArgs e)
+
+        // ======================= CÁC HÀM XỬ LÝ MỞ FORM =======================
+
+        private void CardSchedule_Click(object sender, EventArgs e)
+        {
+            // Mở form Lịch phòng học
+            frmMHLichPhong fLich = new frmMHLichPhong();
+            fLich.Show();
+        }
+
+        private void CardRegister_Click(object sender, EventArgs e)
+        {
+            // Mở form Đăng ký phòng
+            frmMHDKPhong fDangKy = new frmMHDKPhong();
+            fDangKy.Show();
+        }
+
+        private void CardReport_Click(object sender, EventArgs e)
+        {
+            // Mở form Báo cáo
+            frmMHBaoCao fBaoCao = new frmMHBaoCao();
+            fBaoCao.Show();
+        }
+
+        private void cardSchedule_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+    
+
+    private void Form1_Load(object sender, EventArgs e)
+        {
         }
 
         private void frmMHSV_Load(object sender, EventArgs e)
@@ -46,12 +97,7 @@ namespace MHSV
             
         }
 
-        private void cardSchedule_Paint(object sender, PaintEventArgs e)
-        {
-           
-
-        }
-
+      
         private void materialLabel1_Click(object sender, EventArgs e)
         {
 
@@ -93,6 +139,7 @@ namespace MHSV
 
     private void cardNotice_Paint(object sender, PaintEventArgs e)
         {
+
 
         }
     }
